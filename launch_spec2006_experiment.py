1d3d43f1f4d556623b69a4ace94a83cc2932d104
Kelly Nguyen <yllekn@amarillo.cs.ucdavis.edu>
2021-01-06 14:08:28 -0800

    fixed benchmarks

--- launch_spec2006_experiment.py
+++ launch_spec2006_experiment.py
@@ -98,14 +98,13 @@ if __name__ == "__main__":
                        'o3':     ['test'],
                        'timing': ['test']
                       }
-    benchmarks = ["503.bwaves_r", "507.cactuBSSN_r", "508.namd_r", "510.parest_r", "511.povray_r", "519.lbm_r",
-                  "521.wrf_r", "526.blender_r", "527.cam4_r", "538.imagick_r", "544.nab_r", "549.fotonik3d_r",
-                  "554.roms_r", "997.specrand_fr", "603.bwaves_s", "607.cactuBSSN_s", "619.lbm_s", "621.wrf_s",
-                  "627.cam4_s", "628.pop2_s", "638.imagick_s", "644.nab_s", "649.fotonik3d_s", "654.roms_s",
-                  "996.specrand_fs", "500.perlbench_r", "502.gcc_r", "505.mcf_r", "520.omnetpp_r", "523.xalancbmk_r",
-                  "525.x264_r", "531.deepsjeng_r", "541.leela_r", "548.exchange2_r", "557.xz_r", "999.specrand_ir",
-                  "600.perlbench_s", "602.gcc_s", "605.mcf_s", "620.omnetpp_s", "623.xalancbmk_s", "625.x264_s",
-                  "631.deepsjeng_s", "641.leela_s", "648.exchange2_s", "657.xz_s", "998.specrand_is"]
+    benchmarks = ['401.bzip2','403.gcc','410.bwaves','416.gamess','429.mcf',
+                  '433.milc','434.zeusmp','435.gromacs','436.cactusADM',
+                  '437.leslie3d','444.namd','445.gobmk','453.povray',
+                  '454.calculix','456.hmmer','458.sjeng','459.GemsFDTD',
+                  '462.libquantum','464.h264ref','465.tonto','470.lbm',
+                  '471.omnetpp','473.astar','481.wrf','482.sphinx3',
+                  '998.specrand','999.specrand']
 
     for cpu in cpus:
         for size in benchmark_sizes[cpu]:
